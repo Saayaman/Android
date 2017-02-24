e5b4f90bc237c4fbfb097a9062ea1d102e7dc1fa
Saayaman <ayakosayama1987@gmail.com>
2017-02-23 23:53:11 -0800

    修正

--- TabLayout/app/src/main/java/com/ayako_sayama/tablayout/MainActivity.java
+++ TabLayout/app/src/main/java/com/ayako_sayama/tablayout/MainActivity.java
@@ -1,14 +1,13 @@
 package com.ayako_sayama.tablayout;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -24,17 +23,13 @@ public class MainActivity extends FragmentActivity {
         host.setup(this, getSupportFragmentManager(), R.id.content);
 
         TabHost.TabSpec tabSpec1 = host.newTabSpec("tab1");
-        Button button1 = new Button(this);
-        button1.setBackgroundResource(R.drawable.ic_brightness_2_black_24dp);
-        tabSpec1.setIndicator(button1);
+        tabSpec1.setIndicator("Button1");
         Bundle bundle1 = new Bundle();
         bundle1.putString("name", "Tab1");
         host.addTab(tabSpec1, SampleFragment.class, bundle1);
 
         TabHost.TabSpec tabSpec2 = host.newTabSpec("tab2");
-        Button button2 = new Button(this);
-        button2.setBackgroundResource(R.drawable.ic_brightness_low_black_24dp);
-        tabSpec2.setIndicator(button2);
+        tabSpec2.setIndicator("Button2");
         Bundle bundle2 = new Bundle();
         bundle2.putString("name", "Tab2");
         host.addTab(tabSpec2, SampleFragment.class, bundle2);
